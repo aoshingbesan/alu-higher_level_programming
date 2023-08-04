0a5aec608800f22935e91f0fcef9d2e299ac3214
Ademola Oshingbesan <a.oshingbes@alustudent.com>
2023-08-04 01:58:15 -0700

    task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-'''a Python script that takes in a URL and an email,
-sends a POST request to the passed URL with the email
-as a parameter, and displays the body of the
-response (decoded in utf-8)
-'''
-import urllib.request
-import urllib.parse
-import sys
-
+"""
+Python script to send request to given URL and display value of X-Request-Id
+"""
 if __name__ == "__main__":
-    url = sys.argv[1]
-    params = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(params)
-    data = data.encode('ascii')
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode('utf-8'))
+    from urllib import request
+    from sys import argv
+    if argv[1]:
+        with request.urlopen(argv[1]) as response:
+            print(response.getheader('X-Request-Id'))
