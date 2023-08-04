4a43744e3bb0c5d8737d047412f647d6fc5611e2
Ademola Oshingbesan <a.oshingbes@alustudent.com>
2023-08-04 01:56:03 -0700

    advanced task

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-'''Uses github api to print last ten commits to given repo by given user
-'''
-import sys
-import requests
-
+"""
+Python script to list 10 most recent commits
+to repository 'rails' by user 'rails'
+using GitHub API
+"""
 if __name__ == "__main__":
-    repo = sys.argv[1]
-    username = sys.argv[2]
-    headers = {'accept': 'application/vnd.github+json'}
-    data = {'per_page': 10, 'page': 1}
-    url = f"https://api.github.com/repos/{username}/{repo}/commits"
-    response = requests.get(url, headers=headers, params=data)
-    json_data = response.json()
-    for entry in json_data:
-        print(f"{entry.get('sha')}: \
-{entry.get('commit').get('author').get('name')}")
+    import requests
+    from sys import argv
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        argv[2], argv[1])
+    response = requests.get(url)
+    json_list = response.json()
+    count = 0
+    for obj in json_list:
+        if count == 10:
+            break
+        print("{}: {}".format(
+            obj.get('sha'),
+            obj.get('commit').get('author').get('name')))
+        count = count + 1
